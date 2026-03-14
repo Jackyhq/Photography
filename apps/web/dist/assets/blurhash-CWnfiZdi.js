2e5b566141c034a9752ba2c78dfbbab3aa2f599c
Jackyhq <i@jackyw.cn>
2026-03-14 22:00:56 +0000

    feat(web): 新增无人机照片并更新站点地图与订阅源
    添加两张无人机拍摄的照片及其缩略图，并更新 sitemap.xml 和 feed.xml 以包含新内容。同时，优化了构建产物（如压缩 registerSW.js 和 sw.js 脚本）并更新了相关资源文件的哈希引用，以反映前端应用的最新状态。

---
+++ apps/web/dist/assets/blurhash-CWnfiZdi.js
@@ -0,0 +1,42 @@
+import { c as i, j as e } from '../vendor/1-CkNOVE2J.js'
+import { p as r, S as m } from './index-B57REjV0.js'
+import { T as l } from './index-B4ElomvO.js'
+import '../vendor/2-VRqxSGaj.js'
+const d = () => {
+  const s = i.c(1)
+  let t
+  if (s[0] === Symbol.for('react.memo_cache_sentinel')) {
+    const a = r.getPhotos()
+    ;((t = e.jsx(m, {
+      rootClassName: 'h-screen',
+      children: e.jsx('div', { className: 'columns-4 gap-0', children: a.map(c) }),
+    })),
+      (s[0] = t))
+  } else t = s[0]
+  return t
+}
+function c(s) {
+  return e.jsxs(
+    'div',
+    {
+      className: 'group relative m-2',
+      style: { paddingBottom: `${(s.height / s.width) * 100}%` },
+      children: [
+        e.jsx('img', {
+          src: s.thumbnailUrl,
+          alt: s.title,
+          height: s.height,
+          width: s.width,
+          className: 'absolute inset-0',
+        }),
+        s.thumbHash &&
+          e.jsx('div', {
+            className: 'absolute inset-0 opacity-0 group-hover:opacity-100',
+            children: e.jsx(l, { thumbHash: s.thumbHash }),
+          }),
+      ],
+    },
+    s.id,
+  )
+}
+export { d as Component }
